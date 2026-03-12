193e8615133f0ae42fc0d92ee347de997d39c99b
M Sai Kiran <work.sai4107@gmail.com>
2026-03-13 00:03:33 +0530

    03-13-2026 0014-longest-common-prefix Easy Array N/A (0%) N/A (0%)

--- LeetCode/Easy/0014-longest-common-prefix/0014-longest-common-prefix.java
+++ LeetCode/Easy/0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -4,19 +4,16 @@ class Solution {
             int index = 0;
             char ch;
             for(int i = 0; i < strs[0].length(); i++){
+                if(index == strs[0].length()) return strs[0].substring(0,index);
                 ch = strs[0].charAt(index);
-
                 for(int j = 0; j < strs.length; j++){
-                    if(strs[j].length() == index) return strs[0].substring(0,index);
+                    if(index == strs[j].length() ) return strs[0].substring(0,index);
                     if(strs[j].charAt(index) != ch)
                     {
                         return strs[0].substring(0,index);
                     }
                 }
                 index++;
-                if(index == strs[0].length()){
-                    return strs[0].substring(0,index);
-                }
             }
             return strs[0];
         }
