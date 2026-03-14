3b5ea6bed898d8fa35ef578d0618c39ce15f0326
M Sai Kiran <work.sai4107@gmail.com>
2026-03-14 16:35:46 +0530

    03-14-2026 0169-majority-element Easy Array 1 ms (99.83%) 55.6 MB (67.52%)

---
+++ LeetCode/Easy/0169-majority-element/0169-majority-element.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+                    //Moore's Voting Algorithm.
+        int majority = nums[0];
+        int vote = 0;
+        for(int num:nums){
+            if(num == majority){
+                vote++;
+            }
+            else{
+                vote--;
+                if(vote == 0) {
+                    majority = num;
+                    vote = 1;
+                }
+            }
+        }
+        return majority;
+        // Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        // for(int num:nums){
+        //     map.put(num,map.getOrDefault(num,0) + 1);
+        //     if(map.get(num) > nums.length/2){
+        //         return num;
+        //     }
+        // }return -1;
+    }
+}
