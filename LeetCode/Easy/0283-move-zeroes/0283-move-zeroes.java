49453d8e4a6d9602ea8a95b7d4a873dbf2a2bb88
M Sai Kiran <work.sai4107@gmail.com>
2026-03-24 22:30:41 +0530

    03-24-2026 0283-move-zeroes Easy Array 1 ms (99.83%) 48 MB (31.33%)

--- LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.java
+++ LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.java
@@ -1,12 +1,24 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int temp[] = new int[nums.length];
-        int numPointer = 0;
-        int zeroPointer = nums.length-1;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == 0) temp[zeroPointer--] = nums[i];
-            else temp[numPointer++] = nums[i];
+        //Without extra space.
+        int i = 0;
+        int nzp = 0;
+        for(i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                nums[nzp++] = nums[i];
+            }
         }
-        for(int k = 0; k < temp.length; k++) nums[k] = temp[k];
+        while(nzp <= (nums.length - 1)){
+            nums[nzp] = 0;
+            nzp++;
+        }
     }
 }
+        // int temp[] = new int[nums.length];
+        // int numPointer = 0;
+        // int zeroPointer = nums.length-1;
+        // for(int i = 0; i < nums.length; i++){
+        //     if(nums[i] == 0) temp[zeroPointer--] = nums[i];
+        //     else temp[numPointer++] = nums[i];
+        // }
+        // for(int k = 0; k < temp.length; k++) nums[k] = temp[k];
