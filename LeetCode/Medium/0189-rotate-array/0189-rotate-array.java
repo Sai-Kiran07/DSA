d376b104e68cd68547825537f32e865f7c9d2936
M Sai Kiran <work.sai4107@gmail.com>
2026-07-18 12:19:36 +0530

    07-18-2026 0189-rotate-array Medium Array 0 ms (100%) 61.2 MB (55.66%)

--- LeetCode/Medium/0189-rotate-array/0189-rotate-array.java
+++ LeetCode/Medium/0189-rotate-array/0189-rotate-array.java
@@ -1,11 +1,25 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int[] temp = new int[nums.length];
-        for(int i = 0;i < nums.length; i++){
-            temp[(i+k) % nums.length] = nums[i];
-        }
-        for(int i = 0;i < temp.length;i++){
-            nums[i] = temp[i];
+        //O(1) space.
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+        // int[] temp = new int[nums.length];
+        // for(int i = 0;i < nums.length; i++){
+        //     temp[(i+k) % nums.length] = nums[i];
+        // }
+        // for(int i = 0;i < temp.length;i++){
+        //     nums[i] = temp[i];
+        // }
+    }
+    private void reverse(int[] arr, int start, int end){
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++; end--;
         }
     }
 }
+       
