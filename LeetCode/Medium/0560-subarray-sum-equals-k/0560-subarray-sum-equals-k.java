1dc165cc8871df677cc2eb16ea738d856131d5e3
M Sai Kiran <work.sai4107@gmail.com>
2026-07-19 00:02:59 +0530

    07-19-2026 0560-subarray-sum-equals-k Medium Array 20 ms (98.16%) 48.5 MB (79.12%)

--- LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,29 +1,42 @@
 import java.util.*;
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int prefix[] = new int[nums.length];
-        prefix[0] = nums[0];
-        for(int i = 1;i < nums.length;i++){
-            prefix[i] = prefix[i - 1] + nums[i];
-        }
+        // int prefix[] = new int[nums.length];
+        // prefix[0] = nums[0];
+        // for(int i = 1;i < nums.length;i++){
+        //     prefix[i] = prefix[i - 1] + nums[i];
+        // }
+        // int count = 0;
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for(int i = 0; i < prefix.length;i++){
+        //     if(prefix[i] - k == 0) count++;
+        //     if(map.containsKey(prefix[i] - k)) count+= map.getOrDefault(prefix[i] - k,0);
+        //     map.put(prefix[i],map.getOrDefault(prefix[i],0) + 1);
+        // }
+        HashMap<Integer, Integer> prefix = new HashMap<>();
+        int sum = 0;
         int count = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < prefix.length;i++){
-            if(prefix[i] - k == 0) count++;
-            if(map.containsKey(prefix[i] - k)) count+= map.getOrDefault(prefix[i] - k,0);
-            map.put(prefix[i],map.getOrDefault(prefix[i],0) + 1);
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            if(sum == k) count++;
+
+            int diff = sum - k;
+            if(prefix.containsKey(diff)){
+                count += prefix.get(diff);
+            }
+            prefix.merge(sum,1,Integer::sum);
         }
         return count;
     }
 }
-        // int c = 0;
+        // int count = 0;
         // for(int i = 0; i < nums.length; i++){
         //     int sum = 0;
         //     for(int j = i; j < nums.length; j++){
         //         sum += nums[j];
         //         if(sum == k){
-        //             c++;
+        //             count++;
         //         }
         //     }
         // }
-        // return c;
+        // return count;
