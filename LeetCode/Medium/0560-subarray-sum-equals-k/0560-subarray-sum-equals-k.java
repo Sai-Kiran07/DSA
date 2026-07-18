694c78a238fc00db400ab5ea27a047375e75f435
M Sai Kiran <work.sai4107@gmail.com>
2026-07-18 23:24:51 +0530

    07-18-2026 0560-subarray-sum-equals-k Medium Array 21 ms (97.17%) 49.6 MB (5.59%)

--- LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,15 +1,29 @@
+import java.util.*;
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int c = 0;
-        for(int i = 0; i < nums.length; i++){
-            int sum = 0;
-            for(int j = i; j < nums.length; j++){
-                sum += nums[j];
-                if(sum == k){
-                    c++;
-                }
-            }
+        int prefix[] = new int[nums.length];
+        prefix[0] = nums[0];
+        for(int i = 1;i < nums.length;i++){
+            prefix[i] = prefix[i - 1] + nums[i];
         }
-        return c;
+        int count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < prefix.length;i++){
+            if(prefix[i] - k == 0) count++;
+            if(map.containsKey(prefix[i] - k)) count+= map.getOrDefault(prefix[i] - k,0);
+            map.put(prefix[i],map.getOrDefault(prefix[i],0) + 1);
+        }
+        return count;
     }
 }
+        // int c = 0;
+        // for(int i = 0; i < nums.length; i++){
+        //     int sum = 0;
+        //     for(int j = i; j < nums.length; j++){
+        //         sum += nums[j];
+        //         if(sum == k){
+        //             c++;
+        //         }
+        //     }
+        // }
+        // return c;
