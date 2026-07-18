3e6ecc046f911447f000f872061ffd4409394396
M Sai Kiran <work.sai4107@gmail.com>
2026-07-18 23:54:32 +0530

    07-18-2026 0268-missing-number Easy Array 0 ms (100%) 47.6 MB (14.09%)

---
+++ LeetCode/Easy/0268-missing-number/0268-missing-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int ts = n*(n + 1) / 2;
+        int curr = 0;
+        for(int i : nums) curr += i;
+        return ts-curr;
+    }
+}
